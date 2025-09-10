2c9910b4f877dd6456b4d9d5e5bb465bfea048b3
igorlityuev <106660830+igorlityuev@users.noreply.github.com>
2025-09-10 22:35:02 +0200

    Update kartypracy2+2a.cs

--- kartypracy2+2a.cs
+++ kartypracy2+2a.cs
@@ -3,10 +3,11 @@ using System.Security.Cryptography;
 internal class Program
 {
 
-    
-    
+
+
     private static void Main(string[] args)
     {
+        //*2.1
         int a = Convert.ToInt32(Console.ReadLine());
         if (a % 3 == 0)
         {
@@ -18,7 +19,7 @@ internal class Program
             Console.WriteLine("Nie");
         }
 
-
+        //*2.2
         int b = Convert.ToInt32(Console.ReadLine());
 
         if (b / 100 != 0 && b % 17 == 0)
@@ -32,7 +33,7 @@ internal class Program
         }
 
 
-
+        //*2.3
         int wiek = Convert.ToInt32(Console.ReadLine());
 
         if (wiek >= 18)
@@ -46,7 +47,7 @@ internal class Program
         }
 
 
-
+        //*2.4
         int waga = Convert.ToInt32(Console.ReadLine());
         int limit = 20;
 
@@ -61,12 +62,12 @@ internal class Program
         }
 
 
-
+        //*2.5
         int j = Convert.ToInt32(Console.ReadLine());
         int k = Convert.ToInt32(Console.ReadLine());
         int l = Convert.ToInt32(Console.ReadLine());
 
-        if ( (j<l && l<k) || (j > l && l > k) )
+        if ((j < l && l < k) || (j > l && l > k))
         {
             Console.WriteLine("Tak");
 
@@ -79,11 +80,11 @@ internal class Program
 
 
 
-
+        //*2.6
         int a2 = Convert.ToInt32(Console.ReadLine());
         int p = Convert.ToInt32(Console.ReadLine());
 
-        if (())
+        if ((Math.Pow(a2, p) - a) % p == 0)
         {
             Console.WriteLine("Tak");
 
@@ -95,7 +96,7 @@ internal class Program
 
 
 
-
+        //*2.7
         int v = Convert.ToInt32(Console.ReadLine());
         int n = Convert.ToInt32(Console.ReadLine());
         int m = Convert.ToInt32(Console.ReadLine());
@@ -121,7 +122,7 @@ internal class Program
 
 
         //Karty pracy nr2a
-
+        //*2a.1
         int a1 = Convert.ToInt32(Console.ReadLine());
         int b1 = Convert.ToInt32(Console.ReadLine());
 
@@ -135,7 +136,7 @@ internal class Program
             Console.WriteLine("Nie");
         }
 
-
+        //*2a.2
         double a3 = Convert.ToDouble(Console.ReadLine());
         double b3 = Convert.ToDouble(Console.ReadLine());
 
@@ -150,7 +151,7 @@ internal class Program
         }
 
 
-
+        //*2a.3
         double a4 = Convert.ToDouble(Console.ReadLine());
         double b4 = Convert.ToDouble(Console.ReadLine());
         double c4 = Convert.ToDouble(Console.ReadLine());
@@ -165,21 +166,48 @@ internal class Program
             Console.WriteLine("Nie");
         }
 
+
+
+        //*2a.4
+
         double a5 = Convert.ToDouble(Console.ReadLine());
         double b5 = Convert.ToDouble(Console.ReadLine());
         double c5 = Convert.ToDouble(Console.ReadLine());
         double d5 = Convert.ToDouble(Console.ReadLine());
 
-        if (a4 == b4 || a4 == c4 || b4 == c4)
-        {
-            Console.WriteLine("Tak");
 
+        Console.WriteLine(Math.Min(Math.Min(a5, b5), Math.Min(c5, d5)));
+
+
+
+
+        //*2a.5
+
+        int a6 = Convert.ToInt32(Console.ReadLine());
+        int b6 = Convert.ToInt32(Console.ReadLine());
+        int c6 = Convert.ToInt32(Console.ReadLine());
+
+
+        if (a6 + b6 > c6 && a6 + c6 > b6 && b6 + c6 > a6)
+        {
+            Console.WriteLine("TAK");
         }
         else
         {
-            Console.WriteLine("Nie");
+
+            Console.WriteLine("NIE");
         }
 
+        //*2a.6
+
+        int a7 = Convert.ToInt32(Console.ReadLine());
+        int b7 = Convert.ToInt32(Console.ReadLine());
+        int c7 = Convert.ToInt32(Console.ReadLine());
+
+        if (!(a7 + b7 > c7 && a7 + c7 > b7 && b7 + c7 > a7))
+        {
+            Console.WriteLine()
+        }
 
     }
 }
